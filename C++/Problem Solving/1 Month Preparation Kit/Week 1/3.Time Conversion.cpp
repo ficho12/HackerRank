33338ed1e3a3d3191f80d6440296e8147ddd8070
Fiz Rey Armesto <39385273+ficho12@users.noreply.github.com>
2023-10-25 21:47:04 +0200

    Added 3.Time Conversion

---
+++ C++/Problem Solving/1 Month Preparation Kit/Week 1/3.Time Conversion.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    int hour = stoi(s.substr(0, 2));
+    int minute = stoi(s.substr(3, 2));
+    int second = stoi(s.substr(6, 2));
+    string ampm = s.substr(8, 2);
+    if (ampm == "PM") {
+        if (hour != 12) {
+            hour += 12;
+        }
+    } else {
+        if (hour == 12) {
+            hour = 0;
+        }
+    }
+
+    stringstream ss;
+    ss << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minute << ":" << setw(2) << setfill('0') << second << endl;
+    ss >> s;
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
