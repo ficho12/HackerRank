928222084681259360bcdb379e64e998ae67749b
Fiz Rey Armesto <39385273+ficho12@users.noreply.github.com>
2023-10-27 01:36:40 +0200

    Added 3.Drawing Book

---
+++ C++/Problem Solving/1 Month Preparation Kit/Week 2/2.Zig Zag Sequence.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ~ no response on stdout ~ Can't figure out why. Not working on any version of c++ on the hackerrank IDE.
+
+void findZigZagSequence(vector < int > a, int n){
+    sort(a.begin(), a.end());
+    int mid = (n + 1)/2;
+    swap(a[mid], a[n-1]);
+
+    int st = mid + 1;
+    int ed = n - 1;
+    while(st <= ed){
+        swap(a[st], a[ed]);
+        st = st + 1;
+        ed = ed + 1;
+    }
+    for(int i = 0; i < n; i++){
+        if(i > 0) cout << " ";
+        cout << a[i];
+    }
+    cout << endl;
+}
+
+int main() {
+    int n, x;
+    int test_cases;
+    cin >> test_cases;
+
+    for(int cs = 1; cs <= test_cases; cs++){
+        cin >> n;
+        vector < int > a;
+        for(int i = 0; i < n; i++){
+            cin >> x;
+            a.push_back(x);
+        }
+        findZigZagSequence(a, n);
+    }
+}
+
+
+
