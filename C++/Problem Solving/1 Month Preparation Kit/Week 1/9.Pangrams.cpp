da34127ad327e4da9afe53b0e22b5fa444569aac
Fiz Rey Armesto <39385273+ficho12@users.noreply.github.com>
2023-10-26 14:03:03 +0200

    Added 9.Pangram

---
+++ C++/Problem Solving/1 Month Preparation Kit/Week 1/9.Pangrams.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string pangrams(string s) {
+    unordered_set<char> st;
+    for (const auto& c : s) {
+        if (isalpha(c)) {
+            st.insert(tolower(c));
+        }
+    }
+    return st.size() == 26 ? "pangram" : "not pangram";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = pangrams(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
