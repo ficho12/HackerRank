2d4cb3b91cf622aaac30153f69449099fdf8dfd7
Fiz Rey Armesto <39385273+ficho12@users.noreply.github.com>
2023-10-30 18:12:07 +0100

    Added 11.C++ Varuadics

---
+++ C++/C++ Badge/11.C++ Variadics.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+// Enter your code for reversed_binary_value<bool...>()
+template <bool...digits>
+int reversed_binary_value()
+{
+    int binary_value = 0;
+    int bit_position = 1;
+    bool arr[] = {digits...};
+    for (bool digit : arr) {
+        if (digit) {
+            binary_value += bit_position;
+        }
+        bit_position <<= 1;
+    }
+    return binary_value;
+}
+
+template <int n, bool...digits>
+struct CheckValues {
+  	static void check(int x, int y)
+  	{
+    	CheckValues<n-1, 0, digits...>::check(x, y);
+    	CheckValues<n-1, 1, digits...>::check(x, y);
+  	}
+};
+
+template <bool...digits>
+struct CheckValues<0, digits...> {
+  	static void check(int x, int y)
+  	{
+    	int z = reversed_binary_value<digits...>();
+    	std::cout << (z+64*y==x);
+  	}
+};
+
+int main()
+{
+  	int t; std::cin >> t;
+
+  	for (int i=0; i!=t; ++i) {
+		int x, y;
+    	cin >> x >> y;
+    	CheckValues<6>::check(x, y);
+    	cout << "\n";
+  	}
+}
