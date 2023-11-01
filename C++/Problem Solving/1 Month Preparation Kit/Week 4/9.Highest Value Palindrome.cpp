78e7311fe4e16275d01e052a8a610dc2e1f7f4aa
Fiz Rey Armesto <39385273+ficho12@users.noreply.github.com>
2023-11-01 15:10:59 +0100

    Added 10.Lily's Homework

---
+++ C++/Problem Solving/1 Month Preparation Kit/Week 4/9.Highest Value Palindrome.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+// NOT WORKING
+
+/*
+ * Complete the 'highestValuePalindrome' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER n
+ *  3. INTEGER k
+ */
+
+string highestValuePalindrome(string s, int n, int k) {
+    int changes = 0;
+    vector<int> changeIndex;
+    
+    for (int i = 0, j = n - 1; i < j; ++i, --j) {
+        if (s[i] != s[j]) {
+            changeIndex.push_back(i);
+            if (s[i] < s[j]) {
+                s[i] = s[j];
+            } else {
+                s[j] = s[i];
+            }
+            ++changes;
+        }
+    }
+    
+    if (changes > k) {
+        return "-1";
+    }
+    
+    int remainingChanges = k - changes;
+    
+    for (int i = 0, j = n - 1; i < j; ++i, --j) {
+        if (s[i] != '9') {
+            if (changeIndex.empty() || changeIndex.back() != i) {
+                if (remainingChanges >= 2) {
+                    s[i] = s[j] = '9';
+                    remainingChanges -= 2;
+                } else if (remainingChanges >= 1) {
+                    if (changeIndex.size() > 0 && remainingChanges == 1) {
+                        int idx = changeIndex.back();
+                        s[idx] = s[n - 1 - idx] = '9';
+                        remainingChanges--;
+                    } else {
+                        s[i] = s[j] = '9';
+                        --remainingChanges;
+                    }
+                }
+            }
+        }
+    }
+    
+    if (n % 2 == 1 && remainingChanges > 0) {
+        s[n / 2] = '9';
+        --remainingChanges;
+    }
+    
+    if (remainingChanges > 0) {
+        int i = 0, j = n - 1;
+        while (i <= j && remainingChanges > 0) {
+            if (i == j) {
+                s[i] = '9';
+                break;
+            }
+            if (s[i] != '9') {
+                if (changeIndex.empty() || changeIndex.back() != i) {
+                    if (remainingChanges >= 2) {
+                        s[i] = s[j] = '9';
+                        remainingChanges -= 2;
+                    } else {
+                        s[i] = s[j] = max(s[i], s[j]);
+                        --remainingChanges;
+                    }
+                }
+            }
+            ++i;
+            --j;
+        }
+    }
+    
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string s;
+    getline(cin, s);
+
+    string result = highestValuePalindrome(s, n, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
