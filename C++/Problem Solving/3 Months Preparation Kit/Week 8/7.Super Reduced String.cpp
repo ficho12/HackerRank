ac451ecdf517d6b06ce89affd224bbeb7f091e15
Fiz Rey Armesto <39385273+ficho12@users.noreply.github.com>
2024-09-21 23:21:41 +0200

    Added 7.Super Reduced String

---
+++ C++/Problem Solving/3 Months Preparation Kit/Week 8/7.Super Reduced String.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'superReducedString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string superReducedString(string s) {
+
+}
+string superReducedString(string s) {
+    stack<char> st;
+    for (char c : s) {
+        if (!st.empty() && st.top() == c) {
+            st.pop();
+        } else {
+            st.push(c);
+        }
+    }
+    string result;
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+    reverse(result.begin(), result.end());
+    return result.empty() ? "Empty String" : result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = superReducedString(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
